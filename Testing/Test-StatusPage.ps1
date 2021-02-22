b5040870dadd456574828c2480d461c088effde1
Hekku2 <hekku2@gmail.com>
2021-02-22 09:49:52 +0200

    Fix status page script

--- Testing/Test-StatusPage.ps1
+++ Testing/Test-StatusPage.ps1
@@ -7,7 +7,7 @@
     Defaults to 'developer-settings.json'
 #>
 param(
-  [Parameter()][string]$SettingsFile = 'developer-settings.json'
+    [Parameter()][string]$SettingsFile = 'developer-settings.json'
 )
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -16,8 +16,9 @@ Write-Host "Reading settings from file $SettingsFile"
 $settingsJson = Get-Content -Raw -Path $SettingsFile | ConvertFrom-Json
  
 $address = ./Deployment/Get-FunctionUri.ps1 `
-  -ResourceGroup $settingsJson.ResourceGroupName `
-  -FunctionName 'Monitor'
+    -ResourceGroup $settingsJson.ResourceGroupName `
+    -WebAppName "$($settingsJson.ResourceGroupName)-monitor" `
+    -FunctionName 'GetAlertsAndRules'
 
 Write-Host 'Send status check request'
 Write-Host $address
